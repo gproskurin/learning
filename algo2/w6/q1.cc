44263226d443083a02c5a9b0660a57f2b5eac63b
Gennady Proskurin <gpr@mail.ru>
2015-05-11 22:24:10 +0300

    WIP algo2/w6

--- algo2/w6/q1.cc
+++ algo2/w6/q1.cc
@@ -1,9 +1,17 @@
+#include <algorithm>
 #include <array>
+#include <deque>
+#include <future>
 #include <iostream>
+#include <iterator>
+#include <sstream>
+#include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 #include <assert.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 struct literal_t {
 	bool negate;
@@ -15,6 +23,8 @@ typedef std::array<literal_t, 2> clause_t;
 typedef std::vector<clause_t> expr_t;
 
 typedef std::vector<bool> ass_t;
+
+typedef std::unordered_map<size_t, bool> fixed_ass_t;
 
 bool calc_l(const literal_t& l, const ass_t& ass)
 {
@@ -47,14 +57,39 @@ bool calc_e(const expr_t& e, const ass_t& ass)
 	return true;
 }
 
-ass_t gen_rnd_ass(size_t sz)
+template <typename C>
+void calc_e_get_falses(const expr_t& e, const ass_t& ass, C& falses)
 {
-	ass_t ass(sz);
-	for (ass_t::reference b : ass) {
-		if (random() & 1) {
-			b = !b;
+	for (size_t i=0; i<e.size(); ++i) {
+		const clause_t& c = e.at(i);
+		const bool c_val = calc_c(c, ass);
+		if (c_val == false) {
+			falses.push_back(i);
 		}
 	}
+}
+
+int32_t get_rnd(random_data* const rd_buf)
+{
+	int32_t rnd;
+	const auto r = random_r(rd_buf, &rnd);
+	assert(r==0);
+	return rnd;
+}
+
+ass_t gen_rnd_ass(const size_t sz, random_data* const rd_buf, const fixed_ass_t& fix_ass)
+{
+	ass_t ass;
+	ass.reserve(sz);
+	for (size_t varnum=0; varnum<sz; ++varnum) {
+		const auto fa_iter = fix_ass.find(varnum);
+		if (fa_iter!=fix_ass.cend()) {
+			ass.push_back(fa_iter->second);
+		} else {
+			ass.push_back(get_rnd(rd_buf) & 1);
+		}
+	}
+	assert(ass.size()==sz);
 	return ass;
 }
 
@@ -136,21 +171,170 @@ void print_stat(const expr_t& e) {
 	}
 }
 
+class my_log {
+	std::ostream& os_;
+	std::ostringstream os_str_;
+public:
+	my_log(std::ostream& os) : os_(os) {}
+	template <typename T>
+	std::ostream& operator<<(const T& t) {
+		return os_str_ << t;
+	}
+	~my_log() {
+		os_ << os_str_.str() << std::flush;
+	}
+};
+
+std::pair<expr_t, fixed_ass_t> expr_remove_unique_vars(expr_t e)
+{
+	typedef std::unordered_set<size_t> vars_set_t;
+
+	fixed_ass_t fix_ass;
+
+	expr_t new_e;
+	vars_set_t pos, neg;
+
+again:
+	new_e.clear();
+	pos.clear();
+	neg.clear();
+
+	for (const clause_t& c : e) {
+		for (const literal_t& l : c) {
+			if (l.negate) {
+				neg.emplace(l.varnum);
+			} else {
+				pos.emplace(l.varnum);
+			}
+		}
+	}
+
+	for (const clause_t& c : e) {
+		bool copy_cur_clause = true;
+		for (const literal_t& l : c) {
+			const size_t varnum = l.varnum;
+
+			// all occurrences are negative (or none), fix var to be false
+			if (pos.find(varnum)==pos.cend()) {
+				fix_ass.emplace(varnum, false);
+				copy_cur_clause = false;
+				break;
+			}
+
+			// all occurrences are positive (or none), fix var to be true
+			if (neg.find(varnum)==neg.cend()) {
+				fix_ass.emplace(varnum, true);
+				copy_cur_clause = false;
+				break;
+			}
+		}
+		if (copy_cur_clause)
+			new_e.push_back(c);
+	}
+
+
+	assert(e.size() >= new_e.size());
+	if (e.size() > new_e.size()) {
+		e = std::move(new_e);
+		goto again;
+	}
+
+	return std::make_pair(std::move(new_e), std::move(fix_ass));
+}
+
+bool papadimitriou_inner(const size_t ass_sz, const expr_t& e, const fixed_ass_t& fa, const size_t inner_iter)
+{
+	struct random_data rd_buf;
+	char state[32];
+	const auto seed = time(nullptr);// + std::this_thread::get_id();
+	if (initstate_r(seed, state, sizeof(state), &rd_buf) != 0) {
+		static const std::string err = "Error in srandom_r()";
+		std::cerr << err << std::endl;
+		throw std::runtime_error(err);
+	}
+
+	ass_t ass = gen_rnd_ass(ass_sz, &rd_buf, fa);
+
+	for (size_t j=0; j<inner_iter; ++j) {
+		std::deque<size_t> falses;
+		calc_e_get_falses(e, ass, falses);
+		if (falses.empty()) {
+			print_ass_stat(ass);
+			assert(calc_e(e, ass) == true);
+			if (calc_e(e, ass) != true) {
+				my_log(std::cout) << "BAD_PROGRAMMER\n";
+				throw std::runtime_error("bad_programmer");
+			}
+			my_log(std::cout) << "TRUE!!!\n";
+			return true;
+		}
+		if (j % 10 == 0) {
+			my_log(std::cout)
+				<< "iter:" << j << "/" << inner_iter
+				<< " false_clauses:" << falses.size() << "\n";
+		}
+		// negate variables for random false clause
+		const size_t rnd = static_cast<size_t>(get_rnd(&rd_buf)) % falses.size();
+		const size_t false_c_pos = falses.at(rnd);
+		const clause_t& false_c = e.at(false_c_pos);
+		assert(calc_c(false_c, ass) == false);
+		for (const literal_t& l : false_c) {
+			const size_t varnum = l.varnum;
+			ass_t::reference b = ass.at(varnum);
+			b = !b;
+		}
+	}
+	return false;
+}
+
+bool papadimitriou_parallel(size_t ass_sz, const expr_t& e, const fixed_ass_t& fa)
+{
+	const size_t outer_iter = 100;
+	const size_t inner_iter = 2*e.size()*e.size();
+	std::deque< std::future<bool> > fut;
+	for (size_t i=0; i<outer_iter; ++i) {
+		fut.emplace_back(
+			std::async(
+				std::launch::async,
+				papadimitriou_inner, 
+				ass_sz,
+				std::cref(e),
+				std::cref(fa),
+				inner_iter
+			)
+		);
+	}
+
+	while (!fut.empty()) {
+		auto& f = fut.front();
+		const bool res = f.get();
+		if (res==true)
+			return true;
+		fut.pop_front();
+	}
+	my_log(std::cout) << "ALL_FALSE\n";
+	return false;
+}
+
 void run()
 {
-	srandom(time(nullptr));
 	const auto p = load_expr(std::cin);
 	const expr_t& e = p.first;
 	const size_t N = p.second;
 	print_stat(e);
-	for (int i=0; i<10; ++i) {
-		const ass_t ass = gen_rnd_ass(N);
-		print_ass_stat(ass);
-		if (calc_e(e, ass)) {
-			std::cout << "TRUE\n";
-			return;
-		}
-	}
+	std::cout << "\n";
+
+	const auto e_fa = expr_remove_unique_vars(e);
+	const expr_t& new_e = e_fa.first;
+	const fixed_ass_t& fa = e_fa.second;
+	print_stat(new_e);
+	std::cout << "\n";
+
+	const bool res = papadimitriou_parallel(N, new_e, fa);
+	if (res)
+		std::cout << "Result: TRUE!!!\n";
+	else
+		std::cout << "Result: may be false...\n";
 }
 
 int main()
