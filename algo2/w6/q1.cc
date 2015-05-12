6595447e1532090aef16380ccbf8c02209c7788b
Gennady Proskurin <gpr@mail.ru>
2015-05-12 16:50:52 +0300

    WIP algo2/w6 (Complete!)

--- algo2/w6/q1.cc
+++ algo2/w6/q1.cc
@@ -1,13 +1,17 @@
 #include <algorithm>
 #include <array>
+#include <chrono>
 #include <deque>
 #include <future>
 #include <iostream>
 #include <iterator>
+#include <list>
 #include <sstream>
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
+
+#include <boost/optional.hpp>
 
 #include <assert.h>
 #include <stdlib.h>
@@ -17,12 +21,12 @@ struct literal_t {
 	bool negate;
 	size_t varnum;
 };
-
 typedef std::array<literal_t, 2> clause_t;
-
 typedef std::vector<clause_t> expr_t;
 
 typedef std::vector<bool> ass_t;
+
+typedef boost::optional<ass_t> opt_ass_t;
 
 typedef std::unordered_map<size_t, bool> fixed_ass_t;
 
@@ -77,19 +81,25 @@ int32_t get_rnd(random_data* const rd_buf)
 	return rnd;
 }
 
+bool get_rnd_bool(random_data* const rd_buf)
+{
+	if (rd_buf) {
+		const bool r = get_rnd(rd_buf) & 1;
+		return r;
+	}
+	const bool r = random() & 1;
+	return r;
+}
+
 ass_t gen_rnd_ass(const size_t sz, random_data* const rd_buf, const fixed_ass_t& fix_ass)
 {
 	ass_t ass;
 	ass.reserve(sz);
 	for (size_t varnum=0; varnum<sz; ++varnum) {
 		const auto fa_iter = fix_ass.find(varnum);
-		if (fa_iter!=fix_ass.cend()) {
-			ass.push_back(fa_iter->second);
-		} else {
-			ass.push_back(get_rnd(rd_buf) & 1);
-		}
+		const bool b = fa_iter!=fix_ass.cend() ?  fa_iter->second : get_rnd_bool(rd_buf);
+		ass.push_back(b);
 	}
-	assert(ass.size()==sz);
 	return ass;
 }
 
@@ -149,8 +159,15 @@ std::pair<expr_t,size_t> load_expr(std::istream& is)
 		throw std::runtime_error("early_EOF");
 	char c;
 	is >> c;
-	if (c)
+	if (c) {
+		while (is) {
+			std::string line;
+			std::getline(is, line);
+			if (is)
+				std::cout << "EXTRA_LINE: " << line << "\n";
+		}
 		throw std::runtime_error("no_EOF");
+	}
 	return std::make_pair(res, sz);
 }
 
@@ -185,113 +202,145 @@ public:
 	}
 };
 
-std::pair<expr_t, fixed_ass_t> expr_remove_unique_vars(expr_t e)
+std::pair<expr_t,fixed_ass_t> expr_simplify(expr_t e, const size_t V)
 {
 	typedef std::unordered_set<size_t> vars_set_t;
-
 	fixed_ass_t fix_ass;
 
-	expr_t new_e;
-	vars_set_t pos, neg;
+	while (true) {
+		bool improved = false; // did something for optimizationn?
 
-again:
-	new_e.clear();
-	pos.clear();
-	neg.clear();
+		vars_set_t pos, neg;
 
-	for (const clause_t& c : e) {
-		for (const literal_t& l : c) {
-			if (l.negate) {
-				neg.emplace(l.varnum);
+		// collect usage of vars
+		for (const clause_t& c : e) {
+			for (const literal_t& l : c) {
+				assert(l.varnum < V);
+				if (l.negate) {
+					neg.emplace(l.varnum);
+				} else {
+					pos.emplace(l.varnum);
+				}
+			}
+		}
+
+		// Fix unique variables.
+		// If variable does not appear in expression at all, fix its value to something (value doesn't matter)
+		for (size_t vn=0; vn<V; ++vn) {
+			if (pos.find(vn)==pos.cend() && neg.find(vn)==neg.cend() && fix_ass.find(vn)==fix_ass.cend()) {
+				//fix_ass.emplace(vn, true)
+				//fix_ass.emplace(vn, false);
+				fix_ass.emplace(vn, get_rnd_bool(nullptr));
+				improved = true;
+			}
+		}
+
+		expr_t new_e;
+		for (const clause_t& c : e) {
+			bool copy_cur_clause = true;
+			for (const literal_t& l : c) {
+				const size_t vn = l.varnum;
+
+				assert(pos.find(vn)!=pos.cend() || neg.find(vn)!=neg.cend());
+
+				// all occurrences are negative (or none), fix var to be false
+				if (pos.find(vn)==pos.cend()) {
+					assert(neg.find(vn)!=neg.cend());
+					fix_ass.emplace(vn, false);
+					copy_cur_clause = false;
+					break;
+				}
+
+				// all occurrences are positive (or none), fix var to be true
+				if (neg.find(vn)==neg.cend()) {
+					fix_ass.emplace(vn, true);
+					copy_cur_clause = false;
+					break;
+				}
+			}
+			if (copy_cur_clause) {
+				new_e.emplace_back(c);
 			} else {
-				pos.emplace(l.varnum);
+				improved = true; // expression become smaller
 			}
 		}
-	}
 
-	for (const clause_t& c : e) {
-		bool copy_cur_clause = true;
-		for (const literal_t& l : c) {
-			const size_t varnum = l.varnum;
-
-			// all occurrences are negative (or none), fix var to be false
-			if (pos.find(varnum)==pos.cend()) {
-				fix_ass.emplace(varnum, false);
-				copy_cur_clause = false;
-				break;
-			}
-
-			// all occurrences are positive (or none), fix var to be true
-			if (neg.find(varnum)==neg.cend()) {
-				fix_ass.emplace(varnum, true);
-				copy_cur_clause = false;
-				break;
-			}
+		assert(e.size() >= new_e.size());
+		if (new_e.size() < e.size()) {
+			assert(improved);
 		}
-		if (copy_cur_clause)
-			new_e.push_back(c);
-	}
 
-
-	assert(e.size() >= new_e.size());
-	if (e.size() > new_e.size()) {
+		if (!improved) {
+			return std::make_pair(std::move(new_e), std::move(fix_ass));
+		}
 		e = std::move(new_e);
-		goto again;
-	}
-
-	return std::make_pair(std::move(new_e), std::move(fix_ass));
+	} // while (true)
 }
 
-bool papadimitriou_inner(const size_t ass_sz, const expr_t& e, const fixed_ass_t& fa, const size_t inner_iter)
+opt_ass_t papadimitriou_inner(const size_t V, const expr_t& e, const fixed_ass_t& fa, const size_t inner_iter, size_t seed)
 {
 	struct random_data rd_buf;
 	char state[32];
-	const auto seed = time(nullptr);// + std::this_thread::get_id();
 	if (initstate_r(seed, state, sizeof(state), &rd_buf) != 0) {
 		static const std::string err = "Error in srandom_r()";
 		std::cerr << err << std::endl;
 		throw std::runtime_error(err);
 	}
 
-	ass_t ass = gen_rnd_ass(ass_sz, &rd_buf, fa);
+	ass_t ass = gen_rnd_ass(V, &rd_buf, fa);
 
 	for (size_t j=0; j<inner_iter; ++j) {
 		std::deque<size_t> falses;
 		calc_e_get_falses(e, ass, falses);
 		if (falses.empty()) {
-			print_ass_stat(ass);
+			//print_ass_stat(ass);
 			assert(calc_e(e, ass) == true);
-			if (calc_e(e, ass) != true) {
-				my_log(std::cout) << "BAD_PROGRAMMER\n";
-				throw std::runtime_error("bad_programmer");
-			}
-			my_log(std::cout) << "TRUE!!!\n";
-			return true;
+			my_log(std::cout) << "TRUE???\n";
+			return std::move(ass);
 		}
-		if (j % 10 == 0) {
+		#ifndef NDEBUG
+		for (const size_t f_pos : falses) {
+			const clause_t& false_c = e.at(f_pos);
+			assert(calc_c(false_c, ass) == false);
+			for (const literal_t& l : false_c) {
+				assert(calc_l(l, ass) == false);
+				assert(fa.find(l.varnum)==fa.cend());
+			}
+		}
+		#endif
+		#if 1
+		if (j % 100000 == 0) {
 			my_log(std::cout)
 				<< "iter:" << j << "/" << inner_iter
 				<< " false_clauses:" << falses.size() << "\n";
 		}
+		#endif
+
 		// negate variables for random false clause
+		// sometimes not negate, but assign random values
 		const size_t rnd = static_cast<size_t>(get_rnd(&rd_buf)) % falses.size();
 		const size_t false_c_pos = falses.at(rnd);
 		const clause_t& false_c = e.at(false_c_pos);
-		assert(calc_c(false_c, ass) == false);
 		for (const literal_t& l : false_c) {
-			const size_t varnum = l.varnum;
-			ass_t::reference b = ass.at(varnum);
-			b = !b;
+			const size_t vn = l.varnum;
+			assert(fa.find(vn)==fa.cend());
+			ass_t::reference b = ass.at(vn);
+			if (get_rnd_bool(&rd_buf)) {
+				b = !b;
+			} else {
+				b = get_rnd_bool(&rd_buf);
+			}
 		}
 	}
-	return false;
+	return boost::none;
 }
 
-bool papadimitriou_parallel(size_t ass_sz, const expr_t& e, const fixed_ass_t& fa)
+opt_ass_t papadimitriou_parallel(size_t ass_sz, const expr_t& e, const fixed_ass_t& fa)
 {
-	const size_t outer_iter = 100;
-	const size_t inner_iter = 2*e.size()*e.size();
-	std::deque< std::future<bool> > fut;
+	srandom(time(nullptr));
+	const size_t outer_iter = 8; // threads count
+	const size_t inner_iter = 1000000;
+	std::list< std::future<opt_ass_t> > fut;
 	for (size_t i=0; i<outer_iter; ++i) {
 		fut.emplace_back(
 			std::async(
@@ -300,41 +349,62 @@ bool papadimitriou_parallel(size_t ass_sz, const expr_t& e, const fixed_ass_t& f
 				ass_sz,
 				std::cref(e),
 				std::cref(fa),
-				inner_iter
+				inner_iter,
+				random()
 			)
 		);
 	}
+	my_log(std::cout) << "THREADS: " << fut.size() << "\n";
 
+	const auto tm = std::chrono::milliseconds(100);
 	while (!fut.empty()) {
-		auto& f = fut.front();
-		const bool res = f.get();
-		if (res==true)
-			return true;
-		fut.pop_front();
+		auto iter = fut.begin();
+		while (iter != fut.end()) {
+			auto& f = *iter;
+			if (f.wait_for(tm)==std::future_status::timeout) {
+				++iter;
+			} else {
+				opt_ass_t res = f.get();
+				if (res)
+					return std::move(res);
+				iter = fut.erase(iter);
+				my_log(std::cout) << " * threads: " << fut.size() << "\n";
+			}
+		}
 	}
 	my_log(std::cout) << "ALL_FALSE\n";
-	return false;
+	return boost::none;
 }
 
 void run()
 {
 	const auto p = load_expr(std::cin);
 	const expr_t& e = p.first;
-	const size_t N = p.second;
+	const size_t V = p.second; // number of variables
 	print_stat(e);
 	std::cout << "\n";
 
-	const auto e_fa = expr_remove_unique_vars(e);
-	const expr_t& new_e = e_fa.first;
+	const auto e_fa = expr_simplify(e, V);
+	const expr_t& opt_e = e_fa.first;
 	const fixed_ass_t& fa = e_fa.second;
-	print_stat(new_e);
+	std::cout << "vars optimized away (fixed ass): " << fa.size() << " remains:" << (V - fa.size()) << "\n";
 	std::cout << "\n";
 
-	const bool res = papadimitriou_parallel(N, new_e, fa);
-	if (res)
-		std::cout << "Result: TRUE!!!\n";
-	else
+	//const opt_ass_t res = papadimitriou_parallel(V, opt_e, fa);
+	const opt_ass_t res = papadimitriou_parallel(V, opt_e, fa);
+
+	if (res) {
+		const ass_t& ass = res.get();
+		const bool r = calc_e(e, ass);
+		if (r==false) {
+			throw std::runtime_error("BAD_PROGRAMMER_2");
+		} else {
+			std::cout << "Result: TRUE!!!\n";
+			print_ass_stat(ass);
+		}
+	} else {
 		std::cout << "Result: may be false...\n";
+	}
 }
 
 int main()
