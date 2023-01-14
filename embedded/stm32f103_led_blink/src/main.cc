8221868c8cc2adb969a7c2fb1d932b28dd254680
Gennady Proskurin <gpr@mail.ru>
2023-01-14 23:47:55 +0000

    stm32 blinky: big rewrite
    - use cmsis, remove manual registers specification
    - use TIM6 timer, poll its value

--- embedded/stm32f103_led_blink/src/main.cc
+++ embedded/stm32f103_led_blink/src/main.cc
@@ -1,13 +1,17 @@
+//
+
+#if defined TARGET_STM32L152
+#include <cmsis_device_l1/Include/stm32l152xe.h>
+#endif
+
 #include <stdbool.h>
 #include <stdint.h>
+
 
 #if defined TARGET_STM32F100
 	#define GPIOB_BASE 0x40010C00
 	#define GPIO_LED 12 // PB12
 #elif defined TARGET_STM32L152
-	#define GPIOA_BASE 0x40020000
-	//#define GPIOB_BASE 0x40020400
-	//#define GPIO_LED 13 // PB13
 	#define GPIO_LED 5 // PA5
 #else
 	#error
@@ -21,68 +25,25 @@
 	#define RCC_BASE 0x40021000
 	#define RCC_APB2ENR_offset 0x18
 #elif defined TARGET_STM32L152
-	#define RCC_BASE 0x40023800
-	#define RCC_APBENR_offset 0x1C
 #else
 	#error
 #endif
 
-
-#if defined TARGET_STM32F100
-typedef struct {
-	volatile uint32_t CRL;
-	volatile uint32_t CRH;
-	volatile uint32_t IDR;
-	volatile uint32_t ODR;
-	volatile uint32_t BSRR;
-	volatile uint32_t BRR;
-	volatile uint32_t LCKR;
-} gpio_t;
-
-#elif defined TARGET_STM32L152
-
-typedef struct {
-	volatile uint32_t MODER;
-	volatile uint32_t OTYPER;
-	volatile uint32_t OSPEEDR;
-	volatile uint32_t PUPDR;
-	volatile uint32_t IDR;
-	volatile uint32_t ODR;
-	volatile uint32_t BSRR;
-	volatile uint32_t LCKR;
-} gpio_t;
-
+#if defined (TARGET_STM32L152)
+#define NVIC_ADDR_TIM6 0xEC
+#define NVIC_POS_TIM6 43
 #else
-	#error
+#error
 #endif
 
 
-#if defined TARGET_STM32L152
-
-typedef struct {
-	volatile uint32_t CR1;
-	volatile uint32_t CR2;
-	volatile uint32_t _reserved_1;
-	volatile uint32_t DIER;
-	volatile uint32_t SR;
-	volatile uint32_t EGR;
-	volatile uint32_t _reserved_2;
-	volatile uint32_t _reserved_3;
-	volatile uint32_t _reserved_4;
-	volatile uint16_t CNT;
-	volatile uint16_t _reserved_5;
-	volatile uint16_t PSC;
-	volatile uint16_t _reserved_6;
-	volatile uint16_t ARR;
-	volatile uint16_t _reserved_7;
-} basic_timer_t;
-
-#endif
-
-void basic_timer_init(basic_timer_t* t, uint16_t prescaler, uint16_t timeout)
+void basic_timer_init(TIM_TypeDef* const tim, uint16_t prescaler, uint16_t arr)
 {
-	t->PSC = prescaler;
-	t->ARR = timeout;
+	//tim->EGR |= TIM_EGR_UG; // remove
+	tim->PSC = prescaler;
+	tim->ARR = arr;
+	//tim->DIER |= TIM_DIER_UIE; // enable hardware interrupt
+	tim->CR1 = (tim->CR1 & ~(TIM_CR1_UDIS | TIM_CR1_OPM)) | TIM_CR1_CEN;
 }
 
 
@@ -101,10 +62,8 @@ void gpio_set_mode(uint32_t gpio_base_addr, int reg, uint32_t mode, uint32_t cnf
 
 #elif defined TARGET_STM32L152
 
-void gpio_set_mode(uint32_t gpio_base_addr, int reg, uint32_t mode)
+void gpio_set_mode(GPIO_TypeDef* const gpio, int reg, uint32_t mode)
 {
-	gpio_t* const gpio = (gpio_t*) gpio_base_addr;
-
 	const uint32_t mask = mode << (reg * 2);
 	const uint32_t mask_value = mode << (reg * 2);
 	gpio->MODER = (gpio->MODER & ~mask) | mask_value;
@@ -112,17 +71,34 @@ void gpio_set_mode(uint32_t gpio_base_addr, int reg, uint32_t mode)
 #endif
 
 
-
-void gpio_set(uint32_t gpio_base_addr, int reg, bool high)
+void gpio_set(GPIO_TypeDef* const gpio, int reg, bool high)
 {
 	uint32_t const mask = (high ? (1U << reg) : (1U << reg) << 16);
-	((gpio_t*) gpio_base_addr)->BSRR = mask;
+	gpio->BSRR = mask;
 }
 
 
 void delay(int val)
 {
 	for (volatile int i = 0; i<val; ++i) {
+	}
+}
+
+volatile bool led_high = false;
+
+void toggle_led()
+{
+	const bool high = led_high;
+	gpio_set(GPIOA, GPIO_LED, high);
+	led_high = !high;
+}
+
+void IntHandler_Tim6()
+{
+	TIM_TypeDef* const pt = TIM6;
+	if (pt->SR & TIM_SR_UIF) {
+		pt->SR &= ~TIM_SR_UIF;
+		toggle_led();
 	}
 }
 
@@ -136,14 +112,38 @@ void gpio_bus_enable()
 }
 
 #elif defined TARGET_STM32L152
-// enable APB for GPIO to work
 void gpio_bus_enable()
 {
-	// byte access is supported
-	volatile unsigned char* const RCC_APBENR = (unsigned char*) (RCC_BASE + RCC_APBENR_offset);
-	*RCC_APBENR |= 0b01; // enable port A
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN_Msk; // enable port A
+	//RCC->APB1RSTR = RCC_APB1RSTR_TIM6RST_Msk; // reset TIM6
+	RCC->APB1ENR |= RCC_APB1ENR_TIM6EN_Msk; // enable TIM6 // pos=43 prio=50
 }
+
+extern "C" void (*tab[2])(void);
+void nvic_init_tim6()
+{
+	// set interrupt handler
+	*(volatile uint32_t*) NVIC_ADDR_TIM6 = (uintptr_t) &IntHandler_Tim6;
+	tab[43] = &IntHandler_Tim6;
+
+	// enable interrupt
+	//NVIC->ISER[1] = 1U << (NVIC_POS_TIM6 - 32);
+	NVIC_SetPriority(TIM6_IRQn, 3);
+	NVIC_EnableIRQ(TIM6_IRQn);
+}
+
 #endif
+
+void blink(int n)
+{
+	while (n-- > 0) {
+		gpio_set(GPIOA, GPIO_LED, 1);
+		delay(50000);
+		gpio_set(GPIOA, GPIO_LED, 0);
+		delay(50000);
+	}
+}
+
 
 int main()
 {
@@ -152,14 +152,22 @@ int main()
 #if defined TARGET_STM32F100
 	gpio_set_mode(GPIOB_BASE, GPIO_LED, MODE, CNF);
 #elif defined TARGET_STM32L152
-	gpio_set_mode(GPIOA_BASE, GPIO_LED, 0b01);
+	gpio_set_mode(GPIOA, GPIO_LED, 0b01);
 #endif
+	blink(1); delay(300000);
 
-	bool s = true;
+	//nvic_init_tim6();
+	//blink(2); delay(300000);
+	basic_timer_init(TIM6, 1000, 500);
+	blink(1); delay(300000);
+
+	//bool s = true;
 	for (;;) {
-		gpio_set(GPIOA_BASE, GPIO_LED, s);
-		delay(s ? 100000 : 30000);
-		s = !s;
+		//gpio_set(GPIOA_BASE, GPIO_LED, s);
+		//delay(s ? 200000 : 50000);
+		//s = !s;
+		//blink(1); delay(500000);
+		IntHandler_Tim6();
 	}
 	return 0;
 }
--- embedded/stm32f103_led_blink/src/startup.c
+++ embedded/stm32f103_led_blink/src/startup.c
@@ -1,18 +1,33 @@
+#include <stdint.h>
+
+
 extern int main();
 extern void _estack(void);
 
+
 __attribute__((naked, noreturn)) void _reset(void)
 {
-  extern long _sbss, _ebss, _sdata, _edata, _sidata;
-  for (long *src = &_sbss; src < &_ebss; src++) *src = 0;
-  for (long *src = &_sdata, *dst = &_sidata; src < &_edata;) *src++ = *dst++;
+	extern long _sbss, _ebss; // bss
+	extern long _sdata, _edata, _sidata; // data
+	//extern long _stext_flash_to_sram, _etext_flash_to_sram, _stext_sram; // text
 
-  main();
-  for (;;) (void) 0;
+	// zero bss
+	for (long *src = &_sbss; src < &_ebss; src++) *src = 0;
+
+	// copy data to sram
+	for (long *src = &_sdata, *dst = &_sidata; src < &_edata;) *src++ = *dst++;
+
+	// copy text to sram
+	//for (long *src = &_stext_flash_to_sram, *dst = &_stext_sram; src < &_etext_flash_to_sram;)
+	//	*src++ = *dst++;
+
+	main();
+	for (;;);
 }
 
 
-__attribute__((section(".vectors"))) void (*tab[2 /*16 + 68*/])(void) = {
+__attribute__((section(".vectors"))) void (*tab[2 /*16 + 68*/])(void) =
+{
 	_estack, _reset
 };
 
