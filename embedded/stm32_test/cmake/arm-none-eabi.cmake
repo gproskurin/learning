3762f10112e42f43a4202a132870003f3cf68c94
Gennady Proskurin <gpr@mail.ru>
2023-01-28 02:14:01 +0000

    Better crosstools support

---
+++ embedded/stm32_test/cmake/arm-none-eabi.cmake
@@ -0,0 +1,21 @@
+
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(tools "/usr/local/pkg/cross-arm-none-eabi")
+set(CMAKE_C_COMPILER "${tools}/bin/arm-none-eabi-gcc")
+set(CMAKE_C_COMPILER_ID GNU)
+set(CMAKE_C_COMPILER_FORCED TRUE)
+
+set(CMAKE_CXX_COMPILER "${tools}/bin/arm-none-eabi-g++")
+set(CMAKE_CXX_COMPILER_ID GNU)
+set(CMAKE_CXX_COMPILER_FORCED TRUE)
+
+set(OBJCOPY "${tools}/bin/arm-none-eabi-objcopy")
+
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
