7683d5c0e09fdd21ecf4684e9f75aafc86339b68
Gennady Proskurin <gpr@mail.ru>
2023-02-04 20:30:18 +0000

    More portable tools selection

--- embedded/stm32_test/cmake/arm-none-eabi.cmake
+++ embedded/stm32_test/cmake/arm-none-eabi.cmake
@@ -2,17 +2,20 @@
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-#set(tools "/usr/local/pkg/cross-arm-none-eabi")
-set(tools "/usr")
-set(CMAKE_C_COMPILER "${tools}/bin/arm-none-eabi-gcc")
+if (${UNIX} AND ${APPLE})
+	set(tools_prefix_bin "/usr/local/pkg/cross-arm-none-eabi/bin/")
+else()
+	set(tools_prefix_bin "")
+endif()
+set(CMAKE_C_COMPILER "${tools_prefix_bin}arm-none-eabi-gcc")
 set(CMAKE_C_COMPILER_ID GNU)
 set(CMAKE_C_COMPILER_FORCED TRUE)
 
-set(CMAKE_CXX_COMPILER "${tools}/bin/arm-none-eabi-g++")
+set(CMAKE_CXX_COMPILER "${tools_prefix_bin}arm-none-eabi-g++")
 set(CMAKE_CXX_COMPILER_ID GNU)
 set(CMAKE_CXX_COMPILER_FORCED TRUE)
 
-set(OBJCOPY "${tools}/bin/arm-none-eabi-objcopy")
+set(OBJCOPY "${tools_prefix_bin}arm-none-eabi-objcopy")
 
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
