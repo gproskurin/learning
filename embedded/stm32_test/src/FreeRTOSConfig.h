65730927037083e5b258c33516460c0b9ed333cf
Gennady Proskurin <gpr@mail.ru>
2023-02-04 20:30:26 +0000

    Catchup with the latest update of confTICKS

--- embedded/stm32_test/src/FreeRTOSConfig.h
+++ embedded/stm32_test/src/FreeRTOSConfig.h
@@ -19,7 +19,8 @@
 #define configMAX_PRIORITIES				5
 #define configMINIMAL_STACK_SIZE			128
 #define configMAX_TASK_NAME_LEN				16
-#define configUSE_16_BIT_TICKS				0
+//#define configUSE_16_BIT_TICKS				0
+#define configTICK_TYPE_WIDTH_IN_BITS			(TICK_TYPE_WIDTH_32_BITS)
 #define configIDLE_SHOULD_YIELD				0
 #define configUSE_TASK_NOTIFICATIONS			1
 #define configTASK_NOTIFICATION_ARRAY_ENTRIES		3
