1eef9249d28be2d70f19f480fb66a93fe076cdaa
Gennady Proskurin <gpr@mail.ru>
2023-03-11 23:32:22 +0000

    Do not use stdlib

---
+++ embedded/stm32_test/src/my_stdlib.c
@@ -0,0 +1,25 @@
+#include <stddef.h>
+
+
+void* memset(void *dst, int c, size_t n)
+{
+	for (size_t i=0; i<n; ++i) {
+		((char*)dst)[i] = c;
+	}
+	return dst;
+}
+
+void* memcpy(void* restrict dst, const void* restrict src, size_t n)
+{
+	for (size_t i=0; i<n; ++i) {
+		((char*)dst)[i] = ((const char*)src)[i];
+	}
+	return dst;
+}
+
+char *stpcpy(char *dst, const char *src)
+{
+	while ((*dst++ = *src++) != 0) {}
+	return dst-1;
+}
+
