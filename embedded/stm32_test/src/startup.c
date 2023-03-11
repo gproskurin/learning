39917b7471b871f97366ca5a5e2616bdf73b08b3
Gennady Proskurin <gpr@mail.ru>
2023-03-11 23:36:08 +0000

    Small fixes here and there

--- embedded/stm32_test/src/startup.c
+++ embedded/stm32_test/src/startup.c
@@ -10,19 +10,16 @@ extern void IntHandler_Timer();
 
 __attribute__((naked, noreturn)) void _reset(void)
 {
-	extern long _sbss, _ebss; // bss
-	extern long _sdata, _edata, _sidata; // data
-	//extern long _stext_flash_to_sram, _etext_flash_to_sram, _stext_sram; // text
-
 	// zero bss
-	//for (long *src = &_sbss; src < &_ebss; src++) *src = 0;
+	extern long _sbss, _ebss;
 	memset(&_sbss, 0, (const char*)&_ebss - (const char *)&_sbss);
 
 	// copy data to sram
-	//for (long *src = &_sdata, *dst = &_sidata; src < &_edata;) *src++ = *dst++;
+	extern long _sdata, _edata, _sidata;
 	memcpy(&_sdata, &_sidata, (const char*)&_edata - (const char*)&_sdata);
 
 	// copy text to sram
+	//extern long _stext_flash_to_sram, _etext_flash_to_sram, _stext_sram;
 	//for (long *src = &_stext_flash_to_sram, *dst = &_stext_sram; src < &_etext_flash_to_sram;)
 	//	*src++ = *dst++;
 
