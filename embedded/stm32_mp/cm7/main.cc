fcc325be506901e6e7d11ab84593e2b37fffbffe
Gennady Proskurin <gpr@mail.ru>
2024-03-01 22:31:41 +0000

    stm32_mp: WIP display

--- embedded/stm32_mp/cm7/main.cc
+++ embedded/stm32_mp/cm7/main.cc
@@ -3,6 +3,7 @@
 #include "bsp.h"
 #include "logging.h"
 #include "freertos_utils.h"
+#include "ugui.h"
 
 #include "FreeRTOS.h"
 #include "task.h"
@@ -12,7 +13,10 @@
 
 #define PRIO_BLINK 1
 #define PRIO_LCD 2
+#define PRIO_UGUI 2
 #define PRIO_LOGGER 3
+
+#define I2C_TS I2C4
 
 
 #define USART_CON_BAUDRATE 115200
@@ -119,6 +123,22 @@ void init_pin_af(const stm32_lib::gpio::pin_t& pin, uint8_t af)
 }
 
 
+void ts_init()
+{
+	RCC->APB4ENR |= RCC_APB4ENR_I2C4EN_Msk;
+	toggle_bits_10(&RCC->APB4RSTR, RCC_APB4RSTR_I2C4RST_Msk);
+
+	stm32_lib::i2c::init_pins(
+		bsp::lcd::pin_scl, bsp::lcd::af_i2c,
+		bsp::lcd::pin_sda, bsp::lcd::af_i2c
+	);
+
+	I2C_TS->CR1 = 0;
+	//I2C->TIMINGR
+	I2C_TS->CR2 = (1 << I2C_CR2_NBYTES_Pos);
+	I2C_TS->CR1 = I2C_CR1_PE;
+}
+
 
 stm32_lib::lcd_t<480,272> lcd;
 void init()
@@ -177,6 +197,7 @@ void init()
 	lcd.layer1.height = 240;
 	lcd.layer1.reconfig_y();
 
+#if 0
 	lcd.layer2.start_x = 120;
 	lcd.layer2.width = 320;
 	lcd.layer2.reconfig_x();
@@ -184,29 +205,30 @@ void init()
 	lcd.layer2.start_y = 100;
 	lcd.layer2.height = 240;
 	lcd.layer2.reconfig_y();
+	LTDC_Layer2->DCCR = 0x00000000;
+	LTDC_Layer2->CACR = 0xA0;
+	LTDC_Layer2->CFBAR = reinterpret_cast<uint32_t>(fb.data());
+#endif
 
 	static_assert(sizeof(uint32_t) == sizeof(fb.data()));
 	LTDC_Layer1->CFBAR = reinterpret_cast<uint32_t>(fb.data());
-	LTDC_Layer2->CFBAR = reinterpret_cast<uint32_t>(fb.data());
 
 	LTDC_Layer1->DCCR = 0x00000000;
 	LTDC_Layer1->CACR = 0xC0;
-	LTDC_Layer2->DCCR = 0x00000000;
-	LTDC_Layer2->CACR = 0xA0;
 
 	LTDC->BCCR = 0x00FF0000;
 
 	//LTDC->LIPCR = 10000;
-	LTDC->IER = /*LTDC_IER_RRIE |*/ LTDC_IER_TERRIE | LTDC_IER_FUIE /*| LTDC_IER_LIE*/;
+	LTDC->IER = /*LTDC_IER_RRIE | LTDC_IER_TERRIE |*/ LTDC_IER_FUIE /*| LTDC_IER_LIE*/;
 
-	LTDC->SRCR = 0b01; // reload shadow registers
+	lcd.update_v();
 
 	for (size_t i=0; i<fb.size(); i+=3) {
 		std::swap(fb[i], fb[i+2]);
 	}
 
 	lcd.layer1.enable();
-	lcd.layer2.enable();
+	//lcd.layer2.enable();
 	LTDC->GCR |= LTDC_GCR_LTDCEN;
 	logger.log_async("LCD enabled\r\n");
 }
@@ -221,10 +243,11 @@ void task_function_lcd(void*)
 {
 	logger.log_async("LCD task started\r\n");
 	lcd::init();
+	lcd::ts_init();
 	int32_t dx1 = 1;
 	int32_t dy1 = 1;
-	int32_t dx2 = 2;
-	int32_t dy2 = -2;
+	//int32_t dx2 = 2;
+	//int32_t dy2 = -2;
 	for(;;) {
 		uint32_t icr = 0;
 		if (LTDC->ISR & LTDC_ISR_RRIF) {
@@ -259,6 +282,7 @@ void task_function_lcd(void*)
 		lcd::lcd.layer1.start_y += dy1;
 		lcd::lcd.layer1.reconfig_y();
 
+#if 0
 		if (lcd::lcd.layer2.start_x < 2 || lcd::lcd.layer2.start_x > 453)
 			dx2 = -dx2;
 
@@ -269,6 +293,7 @@ void task_function_lcd(void*)
 		lcd::lcd.layer2.reconfig_x();
 		lcd::lcd.layer2.start_y += dy2;
 		lcd::lcd.layer2.reconfig_y();
+#endif
 
 		LTDC->SRCR = 0b10;
 	}
@@ -287,6 +312,111 @@ void create_task_lcd()
 	);
 }
 
+
+freertos_utils::task_data_t<1024> task_data_ugui;
+
+constexpr uint16_t ugui_x = 480/2;
+constexpr uint16_t ugui_y = 272/2;
+std::array<uint8_t, ugui_x*ugui_y*3> fb_ugui = {0};
+UG_GUI ugui;
+
+void fb2_pset(int16_t x, int16_t y, uint32_t color)
+{
+	// framebuffer is in BGR format
+	const size_t idx = 3 * ((ugui_x * y) + x);
+	fb_ugui[idx] = color & 0xff; // B
+	fb_ugui[idx+1] = (color >> 8) & 0xff; // G
+	fb_ugui[idx+2] = (color >> 16) & 0xff; // R
+}
+
+std::array<UG_OBJECT, 3> objbuf1;
+UG_WINDOW w1;
+constexpr uint16_t wx = 10;
+constexpr uint16_t wy = 20;
+constexpr uint16_t bx = 50;
+constexpr uint16_t by = 30;
+
+UG_BUTTON b1;
+constexpr uint8_t b1_id = 1;
+
+UG_BUTTON b2;
+constexpr uint8_t b2_id = 2;
+
+void w_cb(UG_MESSAGE*)
+{
+	logger.log_async("UGUI: window message\r\n");
+}
+
+
+uint32_t r = 314159;
+uint32_t rnd()
+{
+	r = r*69069 + 1;
+	return r;
+}
+
+void task_function_ugui(void*)
+{
+	logger.log_async("UGUI task started\r\n");
+	UG_Init(&ugui, fb2_pset, ugui_x, ugui_y);
+	UG_SelectGUI(&ugui);
+	UG_FontSelect(&FONT_8X12);
+	logger.log_async("UGUI ugui initialized\r\n");
+
+	vTaskDelay(configTICK_RATE_HZ/10);
+
+	lcd::lcd.layer2.start_x = 5;
+	lcd::lcd.layer2.width = ugui_x;
+	lcd::lcd.layer2.reconfig_x();
+	lcd::lcd.layer2.start_y = 5;
+	lcd::lcd.layer2.height = ugui_y;
+	lcd::lcd.layer2.reconfig_y();
+	LTDC_Layer2->CFBAR = reinterpret_cast<uint32_t>(fb_ugui.data());
+	lcd::lcd.layer2.enable();
+	lcd::lcd.update_v();
+
+	UG_WindowCreate(&w1, objbuf1.data(), objbuf1.size(), w_cb);
+	UG_WindowSetTitleText(&w1, "Bzyk bzyk");
+	UG_ButtonCreate(&w1, &b1, b1_id, wx, wy, wx+bx, wy+by);
+	UG_ButtonSetText(&w1, b1_id, "b1");
+	UG_ButtonCreate(&w1, &b2, b2_id, wx+bx+10, wy, wx+bx+10+bx, wy+by);
+	UG_ButtonSetText(&w1, b2_id, "b2");
+	UG_WindowShow(&w1);
+	UG_Update();
+	lcd::lcd.update_v();
+
+	if(0) {
+		for (int i=0; i<ugui_y/2-1; i += 2) {
+			UG_DrawFrame(i, i, (ugui_x-1)-i, (ugui_y-1)-i, rnd() & 0xffffff);
+		}
+		UG_FillCircle(
+			ugui_x/2 + (rnd() % (ugui_x/4)),
+			ugui_y/2 + (rnd() % (ugui_y/4)),
+			rnd() % (ugui_y/8),
+			rnd() & 0xffffff
+		);
+		UG_Update();
+		lcd::lcd.update_v();
+		vTaskDelay(configTICK_RATE_HZ/10);
+	}
+
+	for(;;) {
+		vTaskDelay(configTICK_RATE_HZ);
+	}
+}
+
+void create_task_ugui()
+{
+	task_data_lcd.task_handle = xTaskCreateStatic(
+		&task_function_ugui,
+		"UGUI",
+		task_data_ugui.stack.size(),
+		nullptr,
+		PRIO_UGUI,
+		task_data_ugui.stack.data(),
+		&task_data_ugui.task_buffer
+	);
+}
 
 #if 1
 template <typename Pin>
@@ -351,6 +481,10 @@ __attribute__ ((noreturn)) void main()
 	logger.log_sync("Creating LCD task...\r\n");
 	create_task_lcd();
 	logger.log_sync("Created LCD task\r\n");
+
+	logger.log_sync("Creating UGUI task...\r\n");
+	create_task_ugui();
+	logger.log_sync("Created UGUI task\r\n");
 
 	logger.log_sync("Starting FreeRTOS scheduler\r\n");
 	vTaskStartScheduler();
