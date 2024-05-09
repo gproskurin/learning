eaf159261a584832c684b00f9b234133fec22ffe
Gennady Proskurin <gpr@mail.ru>
2024-05-10 00:24:41 +0100

    lora_pair: gateway: move lora handling from CM4 to CM0

---
+++ embedded/lora_pair/gateway/common/utils.cc
@@ -0,0 +1,30 @@
+#include "utils.h"
+
+#include "bsp.h"
+
+#define PRIO_BLINK 1
+
+
+char prn_halfbyte(uint8_t x)
+{
+	return ((x <= 9) ? '0' : ('A'-10)) + x;
+}
+
+char* printf_byte(uint8_t x, char* buf)
+{
+	buf[0] = prn_halfbyte(x >> 4);
+	buf[1] = prn_halfbyte(x & 0b1111);
+	return buf+2;
+}
+
+void log_async_1(uint8_t x, char* const buf0)
+{
+	auto buf = buf0;
+	buf = printf_byte(x, buf);
+	*buf++ = '\r';
+	*buf++ = '\n';
+	*buf = 0;
+	logger.log_async(buf0);
+}
+
+
---
+++ embedded/lora_pair/gateway/common/utils.h
@@ -0,0 +1,11 @@
+#ifndef __my_common_utils_h_included__
+#define __my_common_utils_h_included__
+
+#include <stdint.h>
+
+
+void log_async_1(uint8_t x, char* const buf);
+
+
+#endif
+
