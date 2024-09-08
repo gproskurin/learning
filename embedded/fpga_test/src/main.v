815e831c808c5069794c5d295244e26f2809bf82
Gennady Proskurin <gpr@mail.ru>
2024-09-08 22:06:07 +0100

    fpga_test: move pwm/blinky to separate module

--- embedded/fpga_test/src/main.v
+++ embedded/fpga_test/src/main.v
@@ -1,40 +1,12 @@
-module top(clk, led1, led2);
-
-input clk;
-output led1;
-output led2;
-
+module top(input wire clk, output wire led1, output wire led2);
 
 // led1
-reg [31:0] clk_count_1 = 32'b0;
-reg led1_r = 0;
-assign led1 = led1_r;
+my_pwm #(.W(27)) pwm_led1(clk, 27'd100000000/13, 27'd10000000/17, led1);
+
 
 // led2
-reg [7:0] clk_div_256;
-wire clk_390khz = ~clk_div_256[7];
-reg [18:0] clk_count_2 = 0;
-
-reg led2_r = 0;
-assign led2 = led2_r;
-
-
-always @ (posedge clk)
-begin
-	// counter for led1
-	if (clk_count_1 < 32'd100000000) begin
-		if (clk_count_1 < 32'd10000000) begin
-			led1_r <= 1;
-		end else begin
-			led1_r <= 0;
-		end
-		clk_count_1 <= clk_count_1 + 1;
-	end else begin
-		clk_count_1 <= 0;
-		led1_r <= 1;
-	end
-end
-
+reg [7:0] clk_div_256 = 0;
+wire clk_390khz = ~clk_div_256[7]; // 100mhz / 256
 
 always @ (posedge clk)
 begin
@@ -42,22 +14,7 @@ begin
 	clk_div_256 <= clk_div_256 + 1;
 end
 
-
-always @ (posedge clk_390khz)
-begin
-	// 100Mhz / 256
-	if (clk_count_2 < 19'd390625) begin
-		if (clk_count_2 < 19'd250000) begin
-			led2_r <= 1;
-		end else begin
-			led2_r <= 0;
-		end
-		clk_count_2 <= clk_count_2 + 1;
-	end else begin
-		clk_count_2 <= 0;
-		led2_r <= 0;
-	end
-end
+my_pwm #(.W(19)) pwm_led2(clk_390khz, 100000000/256, 20000, led2);
 
 endmodule
 
